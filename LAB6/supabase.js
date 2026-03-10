1fc574ab1700b6674e90c445c7c1957a503b485d
ErickaPorras <eralporrasm@edu.uc.ac.cr.com>
2026-03-10 15:55:05 -0600

    Agregar archivos y estilos para el Laboratorio 6

---
+++ LAB6/supabase.js
@@ -0,0 +1,5 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://joihisqbbjbordtoofed.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvaWhpc3FiYmpib3JkdG9vZmVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NzEyMDUsImV4cCI6MjA4NzA0NzIwNX0.QanddnqL8-DT3u9s0irHrO39w6RC6VeAFuThaDtoigE'
+const supabase = createClient(supabaseUrl, supabaseKey)
